1df9733e67cd96188cb0d07618531dc0c2bcc091
Faruqoloyede <oloyedefaruq2@gmail.com>
2024-07-12 18:24:25 +0100

    added box shadow to the books item

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -5,7 +5,7 @@ const Books = () => {
   return (
     <div className='px-6 sm:px-16 py-6'>
       <h2 className='text-black font-poppins font-medium text-2xl text-center sm:text-start'>Your Searched Results</h2>
-      <div className='grid grid-cols-4 gap-10 pt-5 cursor-pointer'>
+      <div className='grid sm:grid-cols-4 grid-cols-1 gap-10 pt-5 cursor-pointer'>
         <div className='bg-white flex flex-col items-center justify-center px-6 py-4 book_items'>
             <img src={book} alt="book" className='w-[100px]' />
             <div className='text-center mt-3 font-poppins'>
