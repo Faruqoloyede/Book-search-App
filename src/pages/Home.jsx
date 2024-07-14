6a09e485d53a2cfc57a05004f749b14e2ae196f8
Faruqoloyede <oloyedefaruq2@gmail.com>
2024-07-15 00:05:03 +0100

    install react-router and added hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import Header from '../components/Header'
 import Hero from '../components/Hero'
-import Books from '../components/Books'
 
 const Home = () => {
   return (
     <div>
-      <Header />
       <Hero />
-      <Books />
     </div>
   )
 }
