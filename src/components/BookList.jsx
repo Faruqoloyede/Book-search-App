f521840d896faa6840e5a180a0ba6e2f1759535e
Faruqoloyede <oloyedefaruq2@gmail.com>
2024-07-16 23:13:31 +0100

    added books manually

---
+++ src/components/BookList.jsx
@@ -0,0 +1,117 @@
+import book from "../assets/book.jpg"
+import { Link } from "react-router-dom"
+
+const BookList = () => {
+  return (
+    <div className='bg-blue-50 px-4 py-6'>
+        <h1 className='text-indigo-500 text-3xl font-bold text-center mb-4'>Searched Books</h1>
+        <div className='grid grid-cols-1 md:grid-cols-4 gap-6'>
+            <div className="bg-white rounded-xl shadow-md p-3 flex flex-col text-center ">
+                <img src={book} alt="" className="object-contain w-full" />
+                <div className="text-center">
+                  <p className="text-gray-600 font-bold text-xl my-2">Hamlet, Revenge!</p>
+                  <p className="mb-2"><span className="font-bold">Author:</span> Micheal Innes</p>
+                  <p className="mb-2"><span className="font-bold">Total Editions</span> 15</p>
+                </div>
+                <div className='border border-gray-100 mb-5'></div>
+                <Link to={""} className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm" >
+                    Read more
+                </Link>
+            </div>
+
+            <div className="bg-white rounded-xl shadow-md p-3 flex flex-col text-center ">
+                <img src={book} alt="" className="object-contain w-full" />
+                <div className="text-center">
+                  <p className="text-gray-600 font-bold text-xl my-2">Hamlet, Revenge!</p>
+                  <p className="mb-2"><span className="font-bold">Author:</span> Micheal Innes</p>
+                  <p className="mb-2"><span className="font-bold">Total Editions</span> 15</p>
+                </div>
+                <div className='border border-gray-100 mb-5'></div>
+                <Link to={""} className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm" >
+                    Read more
+                </Link>
+            </div>
+
+            <div className="bg-white rounded-xl shadow-md p-3 flex flex-col text-center ">
+                <img src={book} alt="" className="object-contain w-full" />
+                <div className="text-center">
+                  <p className="text-gray-600 font-bold text-xl my-2">Hamlet, Revenge!</p>
+                  <p className="mb-2"><span className="font-bold">Author:</span> Micheal Innes</p>
+                  <p className="mb-2"><span className="font-bold">Total Editions</span> 15</p>
+                </div>
+                <div className='border border-gray-100 mb-5'></div>
+                <Link to={""} className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm" >
+                    Read more
+                </Link>
+            </div>
+
+            <div className="bg-white rounded-xl shadow-md p-3 flex flex-col text-center ">
+                <img src={book} alt="" className="object-contain w-full" />
+                <div className="text-center">
+                  <p className="text-gray-600 font-bold text-xl my-2">Hamlet, Revenge!</p>
+                  <p className="mb-2"><span className="font-bold">Author:</span> Micheal Innes</p>
+                  <p className="mb-2"><span className="font-bold">Total Editions</span> 15</p>
+                </div>
+                <div className='border border-gray-100 mb-5'></div>
+                <Link to={""} className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm" >
+                    Read more
+                </Link>
+            </div>
+
+            <div className="bg-white rounded-xl shadow-md p-3 flex flex-col text-center ">
+                <img src={book} alt="" className="object-contain w-full" />
+                <div className="text-center">
+                  <p className="text-gray-600 font-bold text-xl my-2">Hamlet, Revenge!</p>
+                  <p className="mb-2"><span className="font-bold">Author:</span> Micheal Innes</p>
+                  <p className="mb-2"><span className="font-bold">Total Editions</span> 15</p>
+                </div>
+                <div className='border border-gray-100 mb-5'></div>
+                <Link to={""} className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm" >
+                    Read more
+                </Link>
+            </div>
+
+            <div className="bg-white rounded-xl shadow-md p-3 flex flex-col text-center ">
+                <img src={book} alt="" className="object-contain w-full" />
+                <div className="text-center">
+                  <p className="text-gray-600 font-bold text-xl my-2">Hamlet, Revenge!</p>
+                  <p className="mb-2"><span className="font-bold">Author:</span> Micheal Innes</p>
+                  <p className="mb-2"><span className="font-bold">Total Editions</span> 15</p>
+                </div>
+                <div className='border border-gray-100 mb-5'></div>
+                <Link to={""} className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm" >
+                    Read more
+                </Link>
+            </div>
+
+            <div className="bg-white rounded-xl shadow-md p-3 flex flex-col text-center ">
+                <img src={book} alt="" className="object-contain w-full" />
+                <div className="text-center">
+                  <p className="text-gray-600 font-bold text-xl my-2">Hamlet, Revenge!</p>
+                  <p className="mb-2"><span className="font-bold">Author:</span> Micheal Innes</p>
+                  <p className="mb-2"><span className="font-bold">Total Editions</span> 15</p>
+                </div>
+                <div className='border border-gray-100 mb-5'></div>
+                <Link to={""} className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm" >
+                    Read more
+                </Link>
+            </div>
+
+            <div className="bg-white rounded-xl shadow-md p-3 flex flex-col text-center ">
+                <img src={book} alt="" className="object-contain w-full" />
+                <div className="text-center">
+                  <p className="text-gray-600 font-bold text-xl my-2">Hamlet, Revenge!</p>
+                  <p className="mb-2"><span className="font-bold">Author:</span> Micheal Innes</p>
+                  <p className="mb-2"><span className="font-bold">Total Editions</span> 15</p>
+                </div>
+                <div className='border border-gray-100 mb-5'></div>
+                <Link to={""} className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm" >
+                    Read more
+                </Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default BookList
