00cb8c777e0613ca18b4da27488d35ae7f8601a5
Faruqoloyede <oloyedefaruq2@gmail.com>
2024-07-19 09:57:00 +0100

    modified the book list

---
+++ src/book.js
@@ -0,0 +1,60 @@
+import book from "./assets/book.jpg"
+
+export const books = [
+    {
+        id: 1,
+        img: book,
+        title: "Hamlet, Revenge!",
+        author: "Micheal Innes",
+        edition: 15
+    },
+    {
+        id: 2,
+        img: book,
+        title: "shakespare!",
+        author: "olwoyin",
+        edition: 12
+    },
+    {
+        id: 3,
+        img: book,
+        title: "revenge for others",
+        author: "emmanuel",
+        edition: 11
+    },
+    {
+        id: 4,
+        img: book,
+        title: "crises",
+        author: "achimede",
+        edition: 20
+    },
+    {
+        id: 5,
+        img: book,
+        title: "ufogetable",
+        author: "okokwo",
+        edition: 19
+    },
+    {
+        id: 6,
+        img: book,
+        title: "unbeatable",
+        author: "namdu",
+        edition: 22
+    },
+    {
+        id: 7,
+        img: book,
+        title: "crying president",
+        author: "musliu",
+        edition: 2
+    },
+    {
+        id: 8,
+        img: book,
+        title: "The gift",
+        author: "ebire",
+        edition: 1
+    },
+]
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div>
       <Hero />
-      <BookList />
+      <BookList isHome={true} />
     </div>
   )
 }
