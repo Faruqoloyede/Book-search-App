f7d69cbb9e32e703a84066f1f6ad32cd25043131
Faruqoloyede <oloyedefaruq2@gmail.com>
2024-07-19 17:26:25 +0100

    bugs fixed

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import SingleBook from "./SingleBook";
 
 const BookList = ({ isHome = false }) => {
   const [books, setBooks] = useState(null); // Initialize as null
   const [loading, setLoading] = useState(true);
-
+  
   useEffect(() => {
     const getBooks = async () => {
       const url = "https://openlibrary.org/search.json?title=hello";
@@ -31,15 +32,20 @@ const BookList = ({ isHome = false }) => {
     return <p>Loading...</p>; // Handle loading state
   }
   return (
-    <div className="bg-blue-50 px-4 py-6">
+    <div className="bg-blue-50 px-4 py-6 flex flex-col items-center">
       <h1 className="text-indigo-500 text-3xl font-bold text-center mb-4">
-        Searched Books
+        {isHome ? "Recent Books" : "Searched Books"}
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-4 gap-6 my-4">
-        {books && books.map((book, index) => (
+        {isHome ? books && books.slice(0, 8).map((book, index) => (
           <SingleBook key={index} book={book} />
+        )) : books && books.map((book, index)=>(
+            <SingleBook key={index} book={book} />
         ))}
       </div>
+      <Link to={""} className=" h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm">
+              View more
+        </Link>
     </div>
   );
 };
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import bookc from "../assets/book.jpg"
 
 const SingleBook = ({ book }) => {
+  const coverbook = `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`;
   return (
     <>
-        <div className="bg-white rounded-xl shadow-md p-3 flex flex-col text-center">
-              <img src={`https://covers.openlibrary.org/b/id/240727-L.jpg`} alt="" className="object-contain w-full" />
-              <div className="text-center">
+        <div className="bg-white rounded-xl shadow-md flex flex-col items-center p-3">
+            <img src={book.cover_i ? coverbook : bookc} className='max-w-[100%] mx-auto' alt="cover" />
+              <div className='text-center'>
                 <p className="text-gray-600 font-bold text-xl my-2"></p>
                 <p className="mb-2"><span className="font-bold">Author:</span> {book.author_name
                 }</p>
@@ -16,7 +18,7 @@ const SingleBook = ({ book }) => {
               <Link to={`/books/${book.id}`} className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm" >
                   Read more
               </Link>
-          </div>
+        </div>
     </>
   )
 }
